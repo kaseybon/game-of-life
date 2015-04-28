74b0ee499ffcbffde8bbaff58d4ac93dbfaad85c
Kasey Bonifacio <kaseybon@gmail.com>
2015-04-28 13:52:18 -0400

    test: test "template" for new Generation class

--- life-generation.rb
+++ life-generation.rb
@@ -1,28 +1,48 @@
-require_relative 'life-grid'
 require_relative 'life-neighbors'
-require_relative 'life-cell'
 require_relative 'life-fate'
 
-def generation(grid)
-  world = Grid.new.start_grid(grid)
-  next_gen_grid = []
+class Generation
+
+  def initialize(living_cells)
+    @living_cells = living_cells
+  end
+
+  def neighborhood
+    neighborhood = {}
+
+    @living_cells.each do |x|
+      neighbors = Neighbors.new.neighbor_map(x)
+      neighborhood.store(x, neighbors)
+    end
+
+    neighborhood
+  end
+  
+  def evolve
+
+  end
+
+
+
+  # world = Grid.new.start_grid(grid)
+  # next_gen_grid = []
 
   
-  # Loop through each array in the world array
-  world.length.times do |x|
+  # # Loop through each array in the world array
+  # world.length.times do |x|
 
-    # While in each array, loop through each item
-    world[x].length.times do |y|
+  #   # While in each array, loop through each item
+  #   world[x].length.times do |y|
 
-      living_neighbors = Neighbors.new.living_neighbors(x,y,world)
-      fate = Fate.new.cell_fate(world[x][y], living_neighbors)
+  #     living_neighbors = Neighbors.new.living_neighbors(x,y,world)
+  #     fate = Fate.new.cell_fate(world[x][y], living_neighbors)
       
-      next_gen_grid << fate
-    end # end y loop
-  end # end x loop
+  #     next_gen_grid << fate
+  #   end # end y loop
+  # end # end x loop
 
-  # Convert next_gen_grid a 2D array
-  next_gen_grid = next_gen_grid.each_slice(world[0].length).to_a
+  # # Convert next_gen_grid a 2D array
+  # next_gen_grid = next_gen_grid.each_slice(world[0].length).to_a
   
-  return next_gen_grid
+  # return next_gen_grid
 end
--- spec/life-generation_spec.rb
+++ spec/life-generation_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 require_relative '../life-generation'
 
-describe 'generation' do
-  it 'returns next generation of a grid' do 
-    generation = generation(1)
-    expect(generation).to eq(
-      [
-        [0,1,0],
-        [0,1,0],
-        [0,0,0]
-      ]
-    )
+describe Generation do
+
+  it 'maps out a neighborhood' do
+    gen = Generation.new [[0,1], [1,2], [1,3], [2,1]]
+    expect(gen.neighborhood).to eq({
+      [0,1] => [],
+      [1,2] => [],
+      [1,3] => [],
+      [2,1] => []
+    })
   end
+
 end
